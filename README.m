2016b127a8a71623717372e84f079f45d0bdf8aa
juaozzin <112671085+Juaozzin@users.noreply.github.com>
2022-10-03 19:51:30 -0300

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,59 @@
+# menor-maior
+Algoritimo "Maior Menor" 
+
+  
+
+Declaração: 
+
+  
+
+Numero1: (nteiro); 
+
+  
+Numero2: (inteiro); 
+
+
+Numero3: (intero);
+  
+
+Inicio 
+  
+
+Escreva  ("Digite numero 1"); 
+         
+
+Leia(numero1; 
+
+  
+
+Escreva ("Digite numero2"); 
+
+      
+
+Leia(numero2; 
+
+  
+
+SE (numero1>numero2) ENTAO 
+
+   Escreva (" O Maior numero1  maior que numero2 ") 
+         
+
+SENAO 
+
+     SE (numero2>numero1) 
+
+ fim-se
+ 
+SE (numero3 > numero2 ) 
+     
+     Escreva (" O Maior numero2 maior numero1 ") 
+     
+ SENAO 
+
+    Escreva  ( " O nuemro 3 maior que numero 2 " ) 
+    
+
+fim-se 
+     
+fim-algarotimo 
